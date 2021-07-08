111244599d17ad626edf7d6cd4bd09fb82fab9a2
hanjiajianing <60766174+hanjiajianing@users.noreply.github.com>
2021-07-08 08:17:16 +0800

    Update UserService.java

--- src/main/java/com/example/boot05webadmin/admin/service/UserService.java
+++ src/main/java/com/example/boot05webadmin/admin/service/UserService.java
@@ -4,5 +4,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.boot05webadmin.admin.bean.User;
 
 public interface UserService extends IService<User> {
-    public void test();//github测试
+
 }
